adfce9a82cd00eace1b4caef0ed35007fce3b124
TEstebanGQ <tomasestebangonzalezquintero@gmail.com>
2025-10-08 15:55:27 -0500

    feat: :sparkles: Se hace arreglo al nav

--- js/navbar.js
+++ js/navbar.js
@@ -180,3 +180,9 @@ if (typeof window !== 'undefined') {
     setActiveNavLink
   };
 }
+const toggle = document.getElementById('menuToggle');
+const navMenu = document.getElementById('navMenu');
+
+toggle.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
